c3e14b898025b522f68c8241081de72321a49183
Alan babu <babuantony135@gmail.com>
2026-06-17 15:12:08 +0530

    up

--- backend/database.py
+++ backend/database.py
@@ -1,15 +1,17 @@
 # database.py - Shared DB connection configuration
 import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./smilecare.db")
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
+if not DATABASE_URL:
+        raise RuntimeError("DATABASE_URL environment variable is missing!")
 
-if DATABASE_URL.startswith("sqlite"):
-    engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
-else:
-    engine = create_engine(DATABASE_URL)
 
+
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
@@ -19,4 +21,3 @@ def get_db():
         yield db
     finally:
         db.close()
-

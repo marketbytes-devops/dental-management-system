4c42fc052964d18909bda2f27b578373956d8be6
Ashiyana Anwar <ashiyanaanwar8@gmail.com>
2026-06-22 13:15:27 +0530

    supabase implemented

--- backend/database.py
+++ backend/database.py
@@ -3,15 +3,17 @@ import os
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
+from pathlib import Path
 
-load_dotenv()
+env_path = Path(__file__).resolve().parent / ".env"
+load_dotenv(dotenv_path=env_path)
+
 DATABASE_URL = os.getenv("DATABASE_URL")
 if not DATABASE_URL:
     raise RuntimeError("DATABASE_URL environment variable is missing!")
 
+engine = create_engine(DATABASE_URL, connect_args={"sslmode": "require"})
 
-
-engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
